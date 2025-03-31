efe122c471c95f7b30c155627579deb000998b06
EMMY <odejinmiemmanuel@gmail.com>
2025-03-31 11:35:09 +0100

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
 
             <h1 class="logo me-auto"><a href="{{route('welcome')}}"><img src="{{asset('assets/img/logo1.png')}}" class="img-fluid" alt=""></a></h1>
             <!-- Uncomment below if you prefer to use an image logo -->
-            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo1.png" alt="" class="img-fluid"></a>-->
 
             <nav id="navbar" class="navbar">
                 <ul>
