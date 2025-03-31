3eb13bf5a22f76d67b288514523523a70f2a5d14
EMMY <odejinmiemmanuel@gmail.com>
2025-03-31 11:14:19 +0100

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Favicons -->
-        <link href="{{asset('assets/img/favicon.png')}}"  rel="icon">
-        <link href="{{asset('assets/img/favicon.png')}}"  rel="apple-touch-icon">
+        <link href="{{asset('assets/img/logo1.png')}}"  rel="icon">
+        <link href="{{asset('assets/img/logo1.png')}}"  rel="apple-touch-icon">
 
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
