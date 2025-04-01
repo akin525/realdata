c83de5b399d0106714185e83e7d4ce7010fe8a72
EMMY <odejinmiemmanuel@gmail.com>
2025-04-01 12:27:31 +0100

    new

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,9 +32,8 @@
                         <center>
                             <!--                    <h4 class="w3-text-green"><b>&nbsp;&nbsp; &nbsp;&nbsp; <a class="w3-btn w3-green w3-border w3-round-large" href="with.php">Withdraw From MCD Wallet</a>-->
                             <a class="badge badge-gradient-primary" href="{{route('admin/credit')}}">Credit User</a>
-                            <a class="badge badge-gradient-primary" href="#">Withdraw Real Data Sub Wallet</a>
-
-                            <a class="badge badge-gradient-primary" href="{{route('admin/credit')}}">Refund User</a>
+{{--                            <a class="badge badge-gradient-primary" href="#">Withdraw Real Data Sub Wallet</a>--}}
+{{--                            <a class="badge badge-gradient-primary" href="{{route('admin/credit')}}">Refund User</a>--}}
                             <a class="badge badge-gradient-primary" href="{{route('admin/charge')}}">Charge User</a>
 {{--                            <a class="w3-btn w3-green w3-border w3-round-large" href="#">Withdraw MCD Commission</a>--}}
 
