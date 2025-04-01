dbedc72e50647aadd653400dd2021259c41dac0f
EMMY <odejinmiemmanuel@gmail.com>
2025-04-01 13:16:52 +0100

    new

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -38,6 +38,20 @@
 {{--                            <a class="w3-btn w3-green w3-border w3-round-large" href="#">Withdraw MCD Commission</a>--}}
 
                             <!--                            <a class="w3-btn w3-green w3-border w3-round-large" href="method.php">All Payment Method</a>-->
+
+                            <div class="col col-lg border-end">
+                                <div class="mt-3 mt-md-0 py-4 px-3">
+                                    <h5 class="text-muted text-uppercase fs-13">ACCOUNT NUMBER<i class="ri-arrow-up-circle-line text-success fs-18 float-end align-middle"></i></h5>
+                                    <div class="d-flex align-items-center">
+{{--                                        <div class="flex-shrink-0">--}}
+{{--                                            <i class="ri-account-box-fill display-6 text-muted"></i>--}}
+{{--                                        </div>--}}
+                                        <div class="flex-grow-1 ms-3">
+                                            <h2 class="mb-0">9629191287</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </center>
                     </div>
                     </b></h4>  	</div>
